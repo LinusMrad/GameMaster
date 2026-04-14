468a5e1fc8f2f4b588970abcb77d2cbd9bcda64f
LinusMrad <leskovec@live.com>
2026-04-14 10:53:48 +0200

    spara lite mer

--- setup_db.py
+++ setup_db.py
@@ -1,6 +1,38 @@
 # Skapa en vectorstore som databas
 import os
-from langchain_community.document_loader import TextLoader
-from lanchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Chroma
+
+# Hämta API-nyckel
+os.environ["GOOGLE_API_KEY"] = "chat_key"
+
+def skapa_databas():
+    print("Läser in bakgrundsiformation")
+    loader = TextLoader("bakgrundsinformation.md", encoding="utf-8")
+    documents = loader.load()
+
+    # Jag delar upp texten i 600 tecken med lite överlapp
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600,
+        chunk_overlap=100,
+        separators=["\n##", "\n###", "\n\n", "\n", " "]
+    )
+    chunks = text_splitter.split_documents(documents)
+
+    # Skapar embeddings
+    print("Skapa embeddings och sparar i Vectorstore")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embeddings-001")
+
+    # Skapa och spara databasen lokalt i i mappen db_ealdror
+    vector_db = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=".\db_ealdror"
+    )
+
+    print("Klart, mappen 'db_ealdror' har skapats")
+
+if __name__ == "__main__":
+    skapa_databas()
